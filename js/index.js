a1a59eebbaeee1f585a70e9b397c81c541e83394
atloomer <alexloomer1@Alexs-MacBook-Pro.local>
2016-10-25 01:28:44 -0700

    Added scroll appear

--- js/index.js
+++ js/index.js
@@ -34,3 +34,33 @@ $('.year').text(year);
 $(document).ready(function(){
 	$('a').confirmMailto();
 });
+
+// CONTENT FADE IN ON SCROLL
+
+$(document).ready(function() {
+
+  /* Every time the window is scrolled ... */
+  $(window).scroll(function() {
+
+    /* Check the location of each desired element */
+    $('.hideme, .fadeleft').each(function(i) {
+
+      var bottom_of_object = $(this).offset().top + $(this).outerHeight();
+      var bottom_of_window = $(window).scrollTop() + $(window).height();
+
+      /* If the object is completely visible in the window, fade it in */
+      if (bottom_of_window > bottom_of_object) {
+
+        $(this).animate({
+          'opacity': '1'
+        }, 800);
+
+      }
+
+    });
+
+  });
+
+});
+
+// End Fade in on Scroll
