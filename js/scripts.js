d3866ba985066c7dd595158ac128468025ddda14
atloomer <alexloomer1@Alexs-MacBook-Pro.local>
2016-10-27 13:16:50 -0700

    Removed animation
    unsafe scripts? need to do more research

--- js/scripts.js
+++
@@ -1,9 +0,0 @@
-// TWEENMAX ANIMATIONS
-
-// Front page animation
-
-var tl1 = new TimelineMax({ delay: .7 });
-
-tl1.from('#header-photo', .8, {opacity:0});
-tl1.from('#bus-name', .5, {opacity:0});
-tl1.from('#reserve-button', .2, {opacity:0,y:-10});
