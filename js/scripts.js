c15ef5aa08509ebacbf75591956141b103bcc272
atloomer <alexloomer1@Alexs-MacBook-Pro.local>
2017-06-14 20:56:10 -0700

    removed front page animation

--- js/scripts.js
+++
@@ -1,7 +0,0 @@
-// Front Page Animation with Greensock
-
-var tl1 = new TimelineMax({ delay: .4 });
-
-tl1.from('#header-photo', .8, {opacity:0});
-tl1.from('#bus-name', .9, {opacity:0,y:15,ease: Power4.easeOut});
-tl1.from('#reserve-button', .9, {opacity:0,y:15,ease: Power4.easeOut},'=-.85');
